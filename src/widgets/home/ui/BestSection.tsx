6c246e4abb55cdc4048a86cf81f80545a1279a05
cho4u4o <cho4u4o@gmail.com>
2025-07-05 19:33:07 +0900

    feat : implement `BestSection` shows best works

---
+++ src/widgets/home/ui/BestSection.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import Image from 'next/image';
+import { useFetchBestWork } from '../api';
+import type { Work } from '../types';
+
+export default function BestSection() {
+  const { data } = useFetchBestWork();
+
+  return (
+    <div className="bg-m-50 px-normal flex w-screen justify-center py-16">
+      <div className="flex w-full max-w-[1078px] flex-col gap-[30px]">
+        <p className="text-h4 font-medium">감성코어 인기 best 작품 순위</p>
+        {data && (
+          <div className="scrollbar-hide flex gap-4 overflow-x-auto">
+            {data.map(work => (
+              <BestItem key={work.id} {...work} />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+const BestItem = ({ title, authorName, coverImageUrl }: Work) => (
+  <div className="flex w-[203px] flex-shrink-0 flex-col gap-4">
+    <Image
+      src={coverImageUrl}
+      alt={title}
+      height={285}
+      width={203}
+      className="rounded-[10px] object-cover object-center"
+    />
+    <div className="flex flex-col">
+      <p className="text-b1 font-medium">{title}</p>
+      <p className="text-b3 text-gray-500">{authorName}</p>
+    </div>
+  </div>
+);
