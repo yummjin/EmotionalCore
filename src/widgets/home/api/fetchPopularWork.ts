172846d3ee2c9d2c0e71c4b6a17a0129e58d6653
cho4u4o <cho4u4o@gmail.com>
2025-07-05 21:54:19 +0900

    feat : add APIs and hooks for fetching monthly popular works and best authors

---
+++ src/widgets/home/api/fetchPopularWork.ts
@@ -0,0 +1,17 @@
+import { get, REQUEST } from '@/shared/api';
+import { useQuery } from '@tanstack/react-query';
+import type { Work } from '../types';
+
+const fetchPopularWork = async () => {
+  const response = await get<Work[]>({
+    request: REQUEST.MONTHLY_POPULAR_WORK,
+  });
+  return response.data;
+};
+
+export const useFetchPopularWork = () => {
+  return useQuery({
+    queryKey: ['popular-work'],
+    queryFn: fetchPopularWork,
+  });
+};
