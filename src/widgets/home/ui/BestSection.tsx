fd1aecff3e8184f1a03f7eed73bda0f0f745d056
cho4u4o <cho4u4o@gmail.com>
2025-07-05 20:39:42 +0900

    design : fix details

--- src/widgets/home/ui/BestSection.tsx
+++ src/widgets/home/ui/BestSection.tsx
@@ -8,9 +8,11 @@ export default function BestSection() {
   const { data } = useFetchBestWork();
 
   return (
-    <div className="bg-m-50 px-normal flex w-screen justify-center py-16">
+    <div className="bg-m-50 px-normal flex w-screen justify-center py-20">
       <div className="flex w-full max-w-[1078px] flex-col gap-[30px]">
-        <p className="text-h4 font-medium">감성코어 인기 best 작품 순위</p>
+        <p className="text-h2 md:text-h1 font-medium">
+          감성코어 인기 best 작품 순위
+        </p>
         {data && (
           <div className="scrollbar-hide flex gap-4 overflow-x-auto">
             {data.map(work => (
@@ -25,13 +27,14 @@ export default function BestSection() {
 
 const BestItem = ({ title, authorName, coverImageUrl }: Work) => (
   <div className="flex w-[203px] flex-shrink-0 flex-col gap-4">
-    <Image
-      src={coverImageUrl}
-      alt={title}
-      height={285}
-      width={203}
-      className="rounded-[10px] object-cover object-center"
-    />
+    <div className="relative h-[285px] w-[203px]">
+      <Image
+        src={`https://emotioncores.com${coverImageUrl}`}
+        alt={title}
+        fill
+        className="rounded-[10px] object-fill"
+      />
+    </div>
     <div className="flex flex-col">
       <p className="text-b1 font-medium">{title}</p>
       <p className="text-b3 text-gray-500">{authorName}</p>
