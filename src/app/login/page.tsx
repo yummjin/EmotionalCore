d981af5fb4bdbc1e68e2774ead4ff7775c600e7d
cho4u4o <cho4u4o@gmail.com>
2025-07-07 20:10:06 +0900

    design : change title component's gap with contents

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,8 +3,7 @@ import { LoginForm, SocialLoginSection } from '@/widgets/login/ui';
 export default function LoginPage() {
   return (
     <div className="px-normal flex w-screen justify-center py-12">
-      <div className="md:max-w-medium lg:max-w-large flex w-full flex-col items-center justify-center gap-[76px]">
-        <p className="text-h3 font-medium">로그인</p>
+      <div className="flex w-full flex-col items-center justify-center gap-10 md:w-[50%] md:gap-[76px] lg:w-[40%]">
         <LoginForm />
         <SocialLoginSection />
       </div>
