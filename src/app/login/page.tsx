8c9e9d11803a73df3faa87512be8610853cddbec
cho4u4o <cho4u4o@gmail.com>
2025-07-23 14:56:34 +0900

    fix : add suspense to solve build error

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,16 @@
 import { LoginForm, SocialLoginSection } from '@/widgets/login/ui';
+import { Suspense } from 'react';
 
 export default function LoginPage() {
   return (
     <div className="px-normal flex w-screen justify-center py-12">
       <div className="flex w-full flex-col items-center justify-center gap-10 md:w-[50%] md:gap-[76px] lg:w-[40%]">
-        <LoginForm />
-        <SocialLoginSection />
+        <Suspense fallback={<div>로딩 중...</div>}>
+          <LoginForm />
+        </Suspense>
+        <Suspense fallback={<div>로딩 중...</div>}>
+          <SocialLoginSection />
+        </Suspense>
       </div>
     </div>
   );
