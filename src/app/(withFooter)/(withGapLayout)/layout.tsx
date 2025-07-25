3d47ed79be0f46aef51aa58cc672a7911f6bd0ba
cho4u4o <cho4u4o@gmail.com>
2025-07-25 13:13:37 +0900

    style : GapLayout 컴포넌트의 패딩 및 간격 조정

--- src/app/(withFooter)/(withGapLayout)/layout.tsx
+++ src/app/(withFooter)/(withGapLayout)/layout.tsx
@@ -2,8 +2,8 @@ import type { ReactNode } from 'react';
 
 export default function GapLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="px-normal flex w-screen justify-center py-[76px]">
-      <div className="md:max-w-medium lg:max-w-large flex w-full flex-col gap-[76px]">
+    <div className="px-normal flex w-screen justify-center py-10 md:py-[76px]">
+      <div className="md:max-w-medium lg:max-w-large flex w-full flex-col gap-10 md:gap-[76px]">
         {children}
       </div>
     </div>
